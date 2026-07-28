707e50d553851ee71a2a78ae38c56941bf7db4bd
kirilldolzhenkov <k.dolzhenkov@gmail.com>
2026-07-29 01:52:15 +0300

    feat: init http server

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"time"
 
+	"github.com/julienschmidt/httprouter"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	name := params.ByName("name")
+	w.Write([]byte(fmt.Sprintf("Test %s", name)))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/:name", IndexHandler)
+
+	listener, err := net.Listen("tcp", "127.0.0.1:1234")
+	if err != nil {
+		panic(err)
+	}
+
+	server := &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(server.Serve(listener))
 }
